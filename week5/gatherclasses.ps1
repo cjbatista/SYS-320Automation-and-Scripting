a8c6ba7167a1ffe2bb45ccacd204ef9d0e933229
cjbatista <charles.batista@mymail.champlain.edu>
2026-02-25 13:11:35 -0500

    Scraping Commits

---
+++ week5/gatherclasses.ps1
@@ -0,0 +1,21 @@
+﻿function gatherClasses() {
+    $page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.32/Courses2026SP.html
+    $trs = $page.ParsedHtml.getElementsByTagName('tr')
+    $FullTable = @()
+    for ($i = 1; $i -lt $trs.length; $i++) {
+        $tds = $trs[$i].getElementsByTagName('td')
+        if ($tds.length -lt 10) {continue}
+
+        $times = $tds[5].innerText.Split('-')
+
+        $FullTable += [PSCustomObject]@{"Class Code"  = $tds[0].innerText;
+                                        "Title"       = $tds[1].innerText;
+                                        "Days"        = $tds[4].innerText;
+                                        "Time Start"  = $times[0];
+                                        "Time End"    = $times[1];
+                                        "Instructor"  = $tds[6].innerText;
+                                        "Location"    = $tds[9].innerText;
+        }
+    }       
+    return $FullTable
+}           
---
+++ week5/printClasses.ps1
@@ -0,0 +1,17 @@
+﻿. (Join-Path $PSScriptRoot gatherclasses.ps1)
+
+$FullTable = daysTranslator(gatherclasses)
+
+#$FullTable | Where-Object { ($_.Location -eq "FREE 105") -and ($_.days -contains "Wednesday") } |
+#    Sort-Object "Time Start" | `
+ #   Select-Object "Time Start", "Time End", "Class Code"
+
+#$FullTable | 
+#    Where-Object { $_."Class Code" -match '^(SYS|NET|SEC|FOR|CSI|DAT)' } |
+#    Select-Object -Expandproperty Instructor | 
+#    Sort-Object -Unique
+
+$FullTable |
+    Group-Object Instructor |
+    Sort-Object Count -Descending |
+    Select-Object Count, Name
