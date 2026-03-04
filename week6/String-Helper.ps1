d04d6961f810666be7f435ed223aca176089b779
cjbatista <143427062+cjbatista@users.noreply.github.com>
2026-03-03 20:13:19 -0500

    Refactor String-Helper functions for clarity

--- week6/String-Helper.ps1
+++ week6/String-Helper.ps1
@@ -1,58 +1,49 @@
-<# 
-String-Helper
+﻿<# String-Helper
 *************************************************************
    This script contains functions that help with String/Match/Search
    operations. 
 ************************************************************* 
 #>
 
+
 <# ******************************************************
-   Function: getMatchingLines
+   Functions: Get Matching Lines
    Input:   1) Text with multiple lines  
             2) Keyword
-   Output:  Array of lines that contain the keyword
+   Output:  1) Array of lines that contain the keyword
 ********************************************************* #>
 function getMatchingLines($contents, $lookline){
-    
-    $allines = @()
-    $splitted = $contents.split([Environment]::NewLine)
 
-    for ($j = 0; $j -lt $splitted.Count; $j++){  
-        if ($splitted[$j].Length -gt 0){  
-            if ($splitted[$j] -ilike $lookline){ 
-                $allines += $splitted[$j] 
-         }
-      }
+$allines = @()
+$splitted =  $contents.split([Environment]::NewLine)
+
+for($j=0; $j -lt $splitted.Count; $j++){  
+ 
+   if($splitted[$j].Length -gt 0){  
+        if($splitted[$j] -ilike $lookline){ $allines += $splitted[$j] }
+   }
+
 }
 
-    return $allines
+return $allines
 }
 
-<# ******************************
-   Function: checkPassword
-   Input:   A plain-text password string
-   Output:  True if valid, False if not
-****************************** #>
-function checkPassword($password){
-    
-    if ($password.Length -lt 6){
-        Write-Host "Password must be at least 6 characters long."
+function checkPassword($password) {
+    # Check if password is at least 6 characters long
+    if ($password.Length -lt 6) {
         return $false
     }
-    
-    if ($password -notmatch '[a-zA-Z]'){
-        Write-Host "Password must contain at least 1 letter."
+
+    # Check if password contains at least 1 special char, 1 num, and 1 letter
+    # Thought I could use regex here but didn't know how so ChatGPT helped
+    $hasSpecialChar = $password -match '[\W_]'
+    $hasNumber = $password -match '\d'
+    $hasLetter = $password -match '[a-zA-Z]'
+
+    # If everything checks out, return true, if not, then return false
+    if ($hasSpecialChar -and $hasNumber -and $hasLetter) {
+        return $true
+    } else {
         return $false
     }
-    
-    if ($password -notmatch '[0-9]'){
-        Write-Host "Password must contain at least 1 number."
-        return $false
-    }
-    
-    if ($password -notmatch '[^a-zA-Z0-9]'){
-        Write-Host "Password must contain at least 1 special character."
-        return $false
-    }
-    return $true
 }
