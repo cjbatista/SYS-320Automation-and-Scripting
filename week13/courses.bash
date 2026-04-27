940e34a3ac610281e0fcd721ad47128bb06dbdfa
cjbatista <charles.batista@mymail.champlain.edu>
2026-04-27 17:22:52 -0400

    simplify xpath in courses.bash

--- week13/courses.bash
+++ week13/courses.bash
@@ -6,20 +6,17 @@ link="10.0.17.6/Courses2026SP.html"
 # get it with curl and tell curl not to give errors
 fullPage=$(curl -sL "$link")
 
-# Utilizing xmlstarlet tool to extract table from the page
+# Extract table rows using xmlstarlet
 toolOutput=$(echo "$fullPage" | \
 xmlstarlet format --html --recover 2>/dev/null | \
-xmlstarlet select --template --copy-of \
-"//html//body//div//table//tbody//tr" 2>/dev/null)
+xmlstarlet select --template --copy-of "//table//tr")
 
-# Processing HTML with sed
-# 1- Replacing every </tr> with a line break
+# Clean up HTML into semicolon-separated rows
 echo "$toolOutput" | sed 's/<\/tr>/\n/g' | \
-sed -e 's/&amp;//g' | \
-sed -e 's/<tr>//g' | \
-sed -e 's/<tr [^>]*>//g' | \
-sed -e 's/<td[^>]*>//g' | \
-sed -e 's/<\/td>/;/g' | \
-sed -e 's/<[/\]\{0,1\}a[^>]*>//g' | \
-sed -e 's/<[/\]\{0,1\}nobr>//g' \
-> ~/SYS-320Automation-and-Scripting/week13/courses.txt
+                     sed -e 's/&amp;//g' | \
+                     sed -e 's/<tr>//g' | \
+                     sed -e 's/<td[^>]*>//g' | \
+                     sed -e 's/<\/td>/;/g' | \
+                     sed -e 's/<[/\]\{0,1\}a[^>]*>//g' | \
+                     sed -e 's/<[/\]\{0,1\}nobr>//g' \
+                     > courses.txt
