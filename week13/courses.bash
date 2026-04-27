f65b9b9e2303a1249153f9a80c5e2707641c147b
cjbatista <charles.batista@mymail.champlain.edu>
2026-04-27 16:46:05 -0400

    fix xpath - courses.txt now populated

--- week13/courses.bash
+++ week13/courses.bash
@@ -10,15 +10,16 @@ fullPage=$(curl -sL "$link")
 toolOutput=$(echo "$fullPage" | \
 xmlstarlet format --html --recover 2>/dev/null | \
 xmlstarlet select --template --copy-of \
-"//html//body//div//div//table//tr")
+"//html//body//div//table//tbody//tr" 2>/dev/null)
 
 # Processing HTML with sed
 # 1- Replacing every </tr> with a line break
 echo "$toolOutput" | sed 's/<\/tr>/\n/g' | \
 sed -e 's/&amp;//g' | \
 sed -e 's/<tr>//g' | \
+sed -e 's/<tr [^>]*>//g' | \
 sed -e 's/<td[^>]*>//g' | \
 sed -e 's/<\/td>/;/g' | \
 sed -e 's/<[/\]\{0,1\}a[^>]*>//g' | \
 sed -e 's/<[/\]\{0,1\}nobr>//g' \
-> courses.txt
+> ~/SYS-320Automation-and-Scripting/week13/courses.txt
